173cc52ffb8a331ad8aba8ae2a0f8d08876460d7
PILLI VENKATA SAI <venkatasai2920@gmail.com>
2021-11-02 19:55:34 +0530

    forms.py

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render,redirect
 from django.core.paginator import Paginator
 from  django.contrib.auth.models import User
 from . models import Comment, Post, Category
+from . forms import AddForm
+
+def add_post(request):
+    form = AddForm()
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+        
+            return redirect('home')
+        
+
+    context ={'form':form}    
+    return render(request,'blog/add.html',context)
+        
+        
+        
 
 # Create your views here.
 def blog(request):
     cat = Category.objects.all()
-    posts = Post.objects.filter(is_published=True).order_by('posted')
-    r_posts = Post.objects.filter(is_published=True).order_by('posted')[0:3]
+    posts = Post.objects.filter(is_published=True)
+    r_posts = Post.objects.filter(is_published=True).order_by('posted')
     #paginator
     posts= Paginator(posts,5)
     page = request.GET.get('page')
@@ -18,11 +37,16 @@ def blog(request):
 def post(request,title):
     
     cat = Category.objects.all()
-    recent_posts = Post.objects.filter(is_published=True).order_by('posted')[0:3]
-    post = Post.objects.get(title=title)
+    
+    recent_posts = Post.objects.filter(is_published=True).order_by('posted')
+    
+    
+    
+    post = Post.objects.get(slug=title)
+    
     related_posts = Post.objects.filter(category=post.category)
     comments = Comment.objects.filter(post=post)
-    return render(request,'blog/post.html',{'post': post, 'cat': cat , 'comments': comments,'recent_posts': recent_posts, 'related_posts' : related_posts})    
+    return render(request,'blog/post.html',{ 'post': post,'slug': post.slug ,'cat': cat , 'comments': comments,'recent_posts': recent_posts, 'related_posts' : related_posts})    
 
 
 def post_comment(request):
@@ -50,7 +74,10 @@ def search_view(request):
     if request.method == 'GET':
         search = request.GET.get('search')
         posts = Post.objects.filter(description__icontains=search)
+        
         posts= Post.objects.filter(title__icontains=search)
+
+       
         cat = Category.objects.all()
         
         r_posts = Post.objects.filter(is_published=True).order_by('posted')[0:3]
@@ -65,10 +92,12 @@ def get_category(request,cat):
      
     
     
-    categories = Category.objects.all()
+    
     cate = Category.objects.get(name=cat)
+
     
     posts = Post.objects.filter(category=cate)
+    categories = Category.objects.all()
    
         
     r_posts = Post.objects.filter(is_published=True).order_by('posted')[0:3]
